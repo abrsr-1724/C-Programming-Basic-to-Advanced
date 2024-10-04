a49f11fdc318a4b7a097e5add7b899a874f13490
Md. Ashik Billah Refat <63367734+abrsr-1724@users.noreply.github.com>
2024-10-04 09:58:31 +0600

    Array

---
+++ 1.Print_1D_Array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int marks[]={80,90,100,70,65,110};
+	printf("%d\n",marks[0]);
+	printf("%d\n",marks[1]);
+	printf("%d\n",marks[2]);
+	printf("%d\n",marks[3]);
+	printf("%d\n",marks[4]);
+	printf("%d\n",marks[5]);
+
+
+	return 0;
+}
